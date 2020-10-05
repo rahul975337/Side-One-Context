4afdd5c1b2fd35280096d6467025362b7d1de366
rahul975337 <tilwani03@gmail.com>
2020-10-05 18:43:27 +0530

    RESOLVED

--- src/provider.js
+++ src/provider.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
-import PackageContext from './context'
+import React, { useState } from "react";
 
-const provider = props => {
-    const [mission, setMission] = useState({
-        mname = "Go to Russia",
-        agent = 007,
-        accept = "Not accepted"
+import PackageContext from "./context";
 
-    })
-    return (
-        <PackageContext.Provider
-            value={{}}
-        >
-            {props.children}
-        </PackageContext.Provider>
-    )
-}
+const Provider = (props) => {
+  const [mission, setMission] = useState({
+    mname: "Go to Russia",
+    agent: "007",
+    accept: "Not accepted",
+  });
+  return (
+    <PackageContext.Provider
+      value={{
+        data: mission,
+        isMissionAccepted: () => {
+          setMission({ ...mission, accept: "ACCEPTED" });
+        },
+      }}
+    >
+      {props.children}
+    </PackageContext.Provider>
+  );
+};
+export default Provider;
